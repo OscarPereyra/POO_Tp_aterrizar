fa6b199197873710d2ee4b2f2b33df6730260b7f
Oscar <oskar.pererya@gmail.com>
2019-05-18 15:38:08 -0300

    Entrega1

---
+++ TpAterrizar_Entrega1/src/modelo/Fecha.java
@@ -0,0 +1,67 @@
+package modelo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
+
+public class Fecha {
+	private SimpleDateFormat formatoIso8601 = new SimpleDateFormat("yyyy/MM/dd");
+	private SimpleDateFormat formatoLatino = new SimpleDateFormat("dd/MM/yyyy");
+	private SimpleDateFormat formatoNorteamericano = new SimpleDateFormat("MM/dd/yyyy");
+
+	private String regexpIso = "^\\d{4}([\\-/.])(0?[1-9]|1[1-2])\\1(3[01]|[12][0-9]|0?[1-9])$";
+	private String regexpNorte = "^(?:0?[1-9]|1[1-2])([\\-/.])(3[01]|[12][0-9]|0?[1-9])\\1\\d{4}$";
+	private String regexpLat = "^(?:3[01]|[12][0-9]|0?[1-9])([\\-/.])(0?[1-9]|1[1-2])\\1\\d{4}$";
+	public Date fecha = null;
+
+	public Date stringADateIso(String fechaIso) throws ParseException {
+		try {
+			fecha = formatoIso8601.parse(fechaIso);
+		} catch (ParseException ex) {
+			System.out.println("No es un formato compatible");
+		}
+		return fecha;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public Date stringADateLatino(String fechaLatino) throws ParseException {
+		try {
+			fecha = formatoLatino.parse(fechaLatino);
+		} catch (ParseException ex) {
+			System.out.println("No es un formato compatible");
+		}
+		return fecha;
+	}
+
+	public Date stringADateNorteamericano(String fechaNorteamericano) throws ParseException {
+		try {
+			fecha = formatoNorteamericano.parse(fechaNorteamericano);
+		} catch (ParseException ex) {
+			System.out.println("No es un formato compatible");
+		}
+		return fecha;
+	}
+
+	public Date stringADateFlexible(String fechaFlexible) throws ParseException {
+		if (Pattern.matches(regexpIso, fechaFlexible)) {
+			fecha = stringADateIso(fechaFlexible);
+		} else if (Pattern.matches(regexpLat, fechaFlexible)) {
+			fecha = stringADateLatino(fechaFlexible);
+		} else if (Pattern.matches(regexpNorte, fechaFlexible)) {
+			fecha = stringADateNorteamericano(fechaFlexible);
+		}
+		return fecha;
+	}
+
+	public boolean fechaAEsAnteriorAFechaB(Date fechaA, Date fechaB) {
+		return fechaA.before(fechaB);
+	}
+
+	public int diasEntreFechas(Date fecha1, Date fecha2) {
+		return Math.abs((int) ((fecha1.getTime() - fecha2.getTime()) / 86400000));
+	}
+}
---
+++ TpAterrizar_Entrega1/test/test/FechaTest.java
@@ -0,0 +1,117 @@
+package test;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import modelo.Fecha;
+
+public class FechaTest {
+	@Test
+	public void stringADateIso_ConvierteStringAIso() throws ParseException {
+		Fecha nueva = new Fecha();
+		nueva.stringADateIso("2019/12/10");
+		Calendar fechaIso = Calendar.getInstance();
+		fechaIso.set(2019, 12, 10);
+		Assert.assertEquals(fechaIso.getTime(), nueva.getFecha());
+	}
+
+	@Test
+	public void stringADateIso_ConvierteStringErroneoAIso() throws ParseException {
+		Fecha nueva = new Fecha();
+		Assert.assertNull("No es un formato compatible", nueva.stringADateIso("2019/1/aa"));
+	}
+
+	@Test
+	public void stringADateLatino_ConvierteStringALatino() throws ParseException {
+		Fecha nueva = new Fecha();
+		nueva.stringADateLatino("1/02/2019");
+		Calendar fechaLatino = Calendar.getInstance();
+		fechaLatino.set(2019, 02, 01);
+		Assert.assertEquals(fechaLatino.getTime(), nueva.getFecha());
+	}
+
+	@Test
+	public void stringADateLatino_ConvierteStringErroneoALatino() throws ParseException {
+		Fecha nueva = new Fecha();
+		Assert.assertNull("No es un formato compatible", nueva.stringADateLatino("2019/1/aa"));
+	}
+
+	@Test
+	public void stringADateNorteamericano_ConvierteStringANorteamericano() throws ParseException {
+		Fecha nueva = new Fecha();
+		nueva.stringADateNorteamericano("12/31/2019");
+		Calendar fechaNorteamericano = Calendar.getInstance();
+		fechaNorteamericano.set(2019, 12, 31);
+		Assert.assertEquals(fechaNorteamericano.getTime(), nueva.getFecha());
+	}
+
+	@Test
+	public void stringADateNorteamericano_ConvierteStringErroneoANorteamericano() throws ParseException {
+		Fecha nueva = new Fecha();
+		Assert.assertNull("No es un formato compatible", nueva.stringADateNorteamericano("aa/02/2019"));
+	}
+
+	@Test
+	public void stringADateFlexible_ConvierteStringAIso() throws ParseException {
+		Fecha nueva = new Fecha();
+		nueva.stringADateIso("2019/12/01");
+	}
+
+	@Test
+	public void stringADateFlexible_ConvierteStringALatino() throws ParseException {
+		Fecha nueva = new Fecha();
+		nueva.stringADateIso("01/12/2019");
+	}
+
+	@Test
+	public void stringADateFlexible_ConvierteStringANorteamericano() throws ParseException {
+		Fecha nueva = new Fecha();
+		nueva.stringADateIso("31/11/2019");
+	}
+
+	@Test
+	public void stringADateFlexible_ConvierteStringErroneoAIso() throws ParseException {
+		Fecha nueva = new Fecha();
+		Assert.assertNull("No es un formato compatible", nueva.stringADateIso("01/a2/2019"));
+	}
+
+	@Test
+	public void stringADateFlexible_ConvierteStringErroneoALatino() throws ParseException {
+		Fecha nueva = new Fecha();
+		Assert.assertNull("No es un formato compatible", nueva.stringADateIso("01/a2/2019"));
+	}
+
+	@Test
+	public void stringADateFlexible_ConvierteStringErroneoANorteamericano() throws ParseException {
+		Fecha nueva = new Fecha();
+		Assert.assertNull("No es un formato compatible", nueva.stringADateIso("31/a1/2019"));
+	}
+
+	@Test
+	public void fechaAEsAnteriorAFechaB_1DeEnero2019EsAnteriorA1DeFebrero2019() throws ParseException {
+		Fecha nueva = new Fecha();
+		Date fechaA = nueva.stringADateLatino("01/01/2019");
+		Date fechaB = nueva.stringADateLatino("01/02/2019");
+		Assert.assertTrue(nueva.fechaAEsAnteriorAFechaB(fechaA, fechaB));
+	}
+
+	@Test
+	public void fechaAEsAnteriorAFechaB_1DeFebrero2019NoEsAnteriorA1DeEnero2019() throws ParseException {
+		Fecha nueva = new Fecha();
+		Date fechaA = nueva.stringADateLatino("01/02/2019");
+		Date fechaB = nueva.stringADateLatino("01/01/2019");
+		Assert.assertFalse(nueva.fechaAEsAnteriorAFechaB(fechaA, fechaB));
+	}
+
+	@Test
+	public void diasEntreFechas_Entre1DeEnero2019Y4DeEnero2019Hay3Dias() throws ParseException {
+		Fecha nueva = new Fecha();
+		Date fechaA = nueva.stringADateLatino("01/01/2019");
+		Date fechaB = nueva.stringADateLatino("04/01/2019");
+		Assert.assertEquals(3, nueva.diasEntreFechas(fechaA, fechaB));
+	}
+}
